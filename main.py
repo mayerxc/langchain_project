f6efd467f3f61237cd20fc7ecf856c02c66ee35d
Chris Mayer <mayeerxc@gmail.com>
2023-12-01 12:03:01 -0500

    add langchain and openai

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import os
-
 from dotenv import load_dotenv
 
+# from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
 load_dotenv()
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_MODEL = "gpt-3.5-turbo"
+# OPENAI_MODEL = "gpt-4"
+
+
+def main():
+    # llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+    # prompt = input("Please provide a prompt: ")
+    # result = llm.predict(prompt)
+    # print(result)
+
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
+    prompt = input("Please provide a prompt: ")
+    result = llm.predict(prompt)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
